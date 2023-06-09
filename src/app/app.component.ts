ce65e19797cf97021aae171adb1c6997a5c1685b
coquilson1 <i.infante.dacruz@outlook.com>
2023-06-08 22:21:09 -0500

    use objects model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Configuracion } from './models/configuracion';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aprendiendo-angular';
+  public title = 'aprendiendo-angular';
+  public description: string;
+  public color: string;
+  public componentConfig;
   public mostrar_videojuegos: boolean = true;
+  
+
+  constructor(){
+    this.title = Configuracion.titulo;
+    this.description = Configuracion.descripcion;
+    this.color = Configuracion.fondo;
+    this.componentConfig = Component;
+  }
 
 
   public ocultarMostrarVideojuegos(value: boolean){
---
+++ src/app/models/configuracion.ts
@@ -0,0 +1,6 @@
+export var Configuracion = {
+    color: "#eee",
+    fondo: "#ccc",
+    titulo: "Aplicacion con Angular",
+    descripcion: "Aprendiendo Angular con Jorge Infante"
+}
