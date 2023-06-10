7f0624a11e66a1b3329d6556432fce6b9d27f15b
coquilson1 <i.infante.dacruz@outlook.com>
2023-06-10 08:00:32 -0500

    add directives

---
+++ src/app/models/zapatilla.ts
@@ -0,0 +1,28 @@
+export class Zapatillas{
+   
+    /* //Esta es la forma más larga
+    public nombre: string;
+    public marca: string;
+    public color: string;
+    public precio: number;
+    public stock: boolean;
+
+    constructor(nombre: string, marca: string, color: string, precio: number, stock:boolean){
+        this.nombre = nombre;
+        this.marca = marca;
+        this.color = color;
+        this.precio = precio;
+        this.stock = stock;
+    }
+    */ 
+
+    //Esta es la forma mas corta
+    constructor(
+        public nombre: string,
+        public marca: string,
+        public color: string,
+        public precio: number,
+        public stock: boolean
+    ){}
+   
+}
--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -1,10 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Zapatillas } from "../models/zapatilla";
 
 @Component({
     selector: 'zapatillas',
     templateUrl: './zapatillas.component.html'
 })
-export class ZapatillasComponent {
+export class ZapatillasComponent implements OnInit {
     public titulo: string = "Componente de Zapatillas ;)";
-}
+    public zapatillas: Array<Zapatillas>;
+    public marcas: String[];
 
+    constructor(){
+        this.zapatillas = [
+            new Zapatillas('Nike Airmax', 'Nike', 'Blanco',40,true),
+            new Zapatillas('Reebok Classic', 'Reebok', 'Blanco',80,true),
+            new Zapatillas('Nike Runner MD', 'Nike', 'Negro',60,true),
+            new Zapatillas('Adidas Yezzy', 'Adidas', 'Gris',180,false)
+        ];
+        this.marcas = new Array();
+    }    
+
+    ngOnInit(): void {
+        console.log('On Init de Zapatillas');
+        console.log(this.zapatillas);
+        this.getMarcas();
+    }
+
+
+    getMarcas(): void{
+        this.zapatillas.forEach((value,index) =>{
+           if(this.marcas.indexOf(value.marca)<0){
+                this.marcas.push(value.marca);
+            }
+        });
+
+        console.log(this.marcas);
+    }
+
+
+}
